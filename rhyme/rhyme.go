7504d014ec23d57fc91127a46e909f294f94eeed
Chris G <cgathercole@gmail.com>
2016-01-31 18:49:10 +0000

    MODIFIED: picking out syllable details

--- rhyme/rhyme.go
+++ rhyme/rhyme.go
@@ -5,17 +5,28 @@ import (
     "os"
     "strings"
     "fmt"
+    "regexp"
 )
 
 const (
 	SyllableFilename = "./cmudict-0.7b"
 )
 
+type Word struct {
+    Name           string
+    FragmentString string
+    Fragments      []string
+    NumSyllables   int
+    FinalSyllable  string
+}
 
-func readSyllables(filename string) (*map[string][]string) {
+func readSyllables(filename string) (*map[string]Word) {
 
-	words := map[string][]string{}
-	countSyllables := 0
+	words := map[string]Word{}
+	countFragments      := 0
+	countSyllables      := 0
+    syllableRegexp      := regexp.MustCompile(`^[A-Z]+\d+$`)
+    finalSyllableRegexp := regexp.MustCompile(`([A-Z]+\d+(?:[^\d]*))$`)
 
     // Open the file.
     f, _ := os.Open(filename)
@@ -28,13 +39,40 @@ func readSyllables(filename string) (*map[string][]string) {
 			nameAndRemainder := strings.Split(line, "  ")
 			name             := nameAndRemainder[0]
 			remainder        := nameAndRemainder[1]
-			syllables        := strings.Split(remainder, " ")
-			countSyllables = countSyllables + len(syllables)
-			words[name] = syllables
+			fragments        := strings.Split(remainder, " ")
+
+			numSyllables := 0
+			for _,f := range fragments {
+				if syllableRegexp.FindStringSubmatch(f) != nil {
+					numSyllables = numSyllables + 1
+	    		}
+	    	}
+
+	    	if numSyllables == 0 {
+	    		fmt.Println("WARNING: no syllables found for name=", name) 
+	    	}
+
+	    	matches := finalSyllableRegexp.FindStringSubmatch(remainder)
+	    	finalSyllable := ""
+	    	if matches != nil {
+	    		finalSyllable = matches[1]
+	    	} else {
+	    		fmt.Println("WARNING: no final syllable found for name=", name) 
+	    	}
+
+	    	countSyllables = countSyllables + numSyllables
+			countFragments = countFragments + len(fragments)
+			words[name] = Word{
+				Name:           name,
+				FragmentString: remainder,
+				Fragments:      fragments,
+				NumSyllables:   numSyllables,
+				FinalSyllable:  finalSyllable,
+			}
 		}
     }
 
-    fmt.Println("num words = ", len(words), ", num syllables = ", countSyllables) 
+    fmt.Println("num words = ", len(words), ", num fragments = ", countFragments, ", num syllables = ", countSyllables) 
 
     return &words
 }
