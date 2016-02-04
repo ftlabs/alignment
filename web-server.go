4d2449abd9b19bee956b36e24b1d36f16d75af17
Chris G <cgathercole@gmail.com>
2016-02-04 23:09:50 +0000

    MODIFIED: made syllabi global (and created once)

--- web-server.go
+++ web-server.go
@@ -45,13 +45,14 @@ func (rri RhymedResultItems) Len()          int  { return len(rri) }
 func (rri RhymedResultItems) Swap(i, j int)      { rri[i], rri[j] = rri[j], rri[i] }
 func (rri RhymedResultItems) Less(i, j int) bool { return rri[i].FinalSyllable > rri[j].FinalSyllable }
 
+var syllabi = rhyme.ConstructSyllabi("rhyme/cmudict-0.7b")
+
 func rhymeHandler(w http.ResponseWriter, r *http.Request) {
     searchParams := sapi.SearchParams{
         Text:   r.FormValue("text"),
         Source: r.FormValue("source"),
     }
     sapiResult := sapi.Search( searchParams )
-    syllabi    := rhyme.ConstructSyllabi("rhyme/cmudict-0.7b")
 
     riwfsList := []ResultItemWithFinalSyllable{}
 
