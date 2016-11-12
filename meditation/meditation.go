8eaf5d19b2df1cf5652385fbcc536f886dd1795f
Chris G <cgathercole@gmail.com>
2016-11-12 23:01:29 +0000

    grabbing imageurl from capi

--- meditation/meditation.go
+++ meditation/meditation.go
@@ -4,14 +4,17 @@ import (
 	"fmt"
 	"github.com/joho/godotenv"
     "github.com/railsagainstignorance/alignment/rss"
-    // "github.com/railsagainstignorance/alignment/content"
+    "github.com/railsagainstignorance/alignment/content"
 )
 
 func GetHaikusWithImages(maxItems int) *[]*rss.Haiku {
 	items := rss.GenerateItems( maxItems )
 	for i, item := range *items {
-		// capiArticle := content.GetArticle(item.Uuid)
-		fmt.Println("meditation: GetHaikusWithImages: ", i, ") ", item.Title )
+		if item.ImageUrl == "" && item.Uuid != "" {
+			capiArticle := content.GetArticle(item.Uuid)
+			item.ImageUrl = capiArticle.ImageUrl
+		}
+		fmt.Println("meditation: GetHaikusWithImages: ", i, ") ", item.Title, ", imageUrl=", item.ImageUrl )
 	}
 	return items
 }
